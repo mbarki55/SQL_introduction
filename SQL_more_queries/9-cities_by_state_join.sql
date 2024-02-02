081f86c020a38cfa0f55550160c71899f07dd068
AITMBAREK oumaima <aitmbarek2oumaima@gmail.com>
2024-02-02 13:57:25 +0100

    task 9*

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
--- Selecting the necessary columns from the cities and states tables
-SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa.cities
-JOIN hbtn_0d_usa.states ON cities.state_id = states.id
-ORDER BY  ASC;
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name AS name, 
+states.name AS name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
